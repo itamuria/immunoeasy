574e18dd8f49bf41c7a04837c2c037c3bdb6c80b
Ibon Tamayo Uria <itamuria@gmail.com>
2020-01-27 13:43:54 +0100

    Create counts2fpkm_subread.R
    Creating function

---
+++ R/counts2fpkm_subread.R
@@ -0,0 +1,104 @@
+#' Title
+#'
+#' @param filename The name of the file with subread counts
+#' @param symbol
+#' @param skiplines
+#' @param mfl_num
+#'
+#' @return
+#' @export
+#'
+#' @examples
+counts2fpkm_subread <- function(filename, symbol = FALSE, skiplines = 1, mfl_num = c(mfl_number))
+{
+  # Load data  ------------------------------------------------------------
+
+  dat <- read.table(filename, header = T)
+  dat <- dat[,c("Geneid", names(dat)[ncol(dat)])]
+  names(dat) <- c("GeneID","Counts")
+
+  # Obtain symbols and length  ------------------------------------------------------------
+
+  library("biomaRt")
+  annotLookup2 <- ens2symbol(vector_names = dat$GeneID)
+
+  # temp <- annotLookup2
+
+  # Merging   ------------------------------------------------------------
+
+  dat$GeneID <- gsub('\\..+$', '', dat$GeneID)
+
+  # Remove duplicate agregating as sum of copies
+  masde1 <- names(table(dat$GeneID)[table(dat$GeneID)>1])
+  temp3 <- dat[dat$GeneID%in%masde1,]
+  temp3b <- aggregate(temp3$Counts, by = list(temp3$GeneID), sum)
+  unique_name <- unique(temp3$GeneID)
+  dat2 <- dat[-which(dat$GeneID%in%unique_name),]
+  names(temp3b) <- names(dat)
+  dat <- rbind(dat2, temp3b)
+  # print(temp3)
+
+  rownames(dat) <- dat[,1]
+
+  gene_names2 <- merge(dat, annotLookup2, by.x = "GeneID", by.y = "ensembl_gene_id")
+
+
+  # Prepare for FPKM ------------------------------------------------------------
+
+  library(countToFPKM)
+
+  # repetir si solo hay una columna de conteos
+  mfl <- c(rep(mfl_num,2))
+
+  # annot3$ensembl_gene_id[annot3$ensembl_gene_id%in%rownames(total4b)]
+  nam_delete <- names(table(gene_names2$ensembl_gene_id)[table(gene_names2$ensembl_gene_id)>1])
+
+  # print(nam_delete)
+
+  gene_names2 <- unique(gene_names2)
+
+
+  gene_names3 <- gene_names2[!gene_names2$ensembl_gene_id%in%nam_delete,]
+
+  temp6 <- gene_names2[gene_names2$ensembl_gene_id%in%nam_delete,]
+  temp6$GeneID <- paste0(temp6$GeneID,c("a","b"))
+  gene_names4 <- rbind(gene_names3,temp6)
+
+  df_calc <- data.frame(gene_names4$Counts,gene_names4$Counts)
+  rownames(df_calc) <- gene_names4$GeneID
+  colnames(df_calc) <- c("Sample_1","Sample_2")
+
+  ## fpkm ------------------------------------------------------------
+
+  fpkm_matrix <- countToFPKM::fpkm (df_calc, gene_names4$length, mfl)
+  dim(fpkm_matrix)
+
+  table(apply(fpkm_matrix, 1, sum) == 0)
+
+  # save(fpkm_matrix, file="Cuartiles.RData")
+
+  fpkm_matrix2 <- data.frame(fpkm_matrix)
+  fpkm_matrix2$Gene_id <- rownames(fpkm_matrix)
+  x <- fpkm_matrix2
+
+  for(g in 1:2)
+  {
+    temp1 <- x[,g]
+    qt <- quantile(temp1[temp1!=0], probs=0:4/4)
+    q1 <- as.numeric(qt[1])
+    q2 <- as.numeric(qt[2])
+    q3 <- as.numeric(qt[3])
+    q4 <- as.numeric(qt[4])
+
+    qtal <- ifelse(temp1 <= q2, 1,
+                   ifelse(temp1 <= q3 & temp1 > q2, 2,
+                          ifelse(temp1 <= q4 & temp1 > q3, 3,
+                                 ifelse(temp1 > q4, 4, NA))))
+    table(qtal)
+    x[,paste0("Cuartiles_",names(x)[g])] <- qtal
+    qtal <- NA
+  }
+
+  x3 <- merge(x, gene_names4, by.x = "Gene_id", by.y = "GeneID")
+  return(x3)
+}
