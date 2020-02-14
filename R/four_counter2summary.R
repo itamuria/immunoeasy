fb412f26a32087b17be4ae6555f46b6344bc8c7a
Ibon Tamayo Uria <itamuria@gmail.com>
2020-02-14 12:02:16 +0100

    summary how many + others

--- R/four_counter2summary.R
+++ R/four_counter2summary.R
@@ -8,6 +8,7 @@
 #' @param mfl_number Mean length
 #' @param export_excel_name Name of the final excel name
 #' @param save_intermediate If we want to save intermediate files
+#' @param dif_cuartiles If we want to calculate the differences between cuartiles
 #'
 #' @return data frame with all the information merged
 #' @export
@@ -21,18 +22,13 @@
 #' }
 #'
 four_counter2summary <- function(semi_subread,semi_cuff,semi_quant,semi_htseq,
-                                ngenes = NULL, mfl_number = mfl_number,
+                                ngenes = NULL,
                                 export_excel_name = "four_together.xlsx",
-                                save_final = FALSE)
+                                save_final = FALSE, dif_cuartiles = FALSE)
 {
   names(semi_cuff) <- c("Gene_id","Symbol","cuff_locus","cuff_FPKM","cuff_Cuartiles")
   semi_cuffb10 <- from_multinames_to_rows (dataset = semi_cuff, colu_name = "Symbol")
   semi_cuffb10 <- semi_cuffb10[,-which(names(semi_cuffb10)=="delete")]
-
-  # names(semi_cuff2) <- c("Gene_id","Symbol","cuff2_locus","cuff2_FPKM","cuff2_Cuartiles")
-  # semi_cuff2b10 <- from_multinames_to_rows (dataset = semi_cuff2, colu_name = "Symbol")
-  # semi_cuff2b10 <- semi_cuff2b10[,-which(names(semi_cuff2b10)=="delete")]
-  # semi_cuff2b10 <- semi_cuff2b10[which(semi_cuff2b10$cuff2_locus%in%grep("^C|^G",semi_cuff2b10$cuff2_locus, value=TRUE, invert = TRUE)),]
 
   semi_htseq2 <- semi_htseq[,c(1,6,5,2,4)]
   semi_htseq2$ht_locus <- paste0(semi_htseq$chromosome_name,":",semi_htseq$start_position,"-",semi_htseq$end_position)
@@ -44,7 +40,7 @@ four_counter2summary <- function(semi_subread,semi_cuff,semi_quant,semi_htseq,
 
   semi_subread2 <- semi_subread[,c(1,6,5,2,4)]
   semi_subread2$ht_locus <- paste0(semi_subread$chromosome_name,":",semi_subread$start_position,"-",semi_subread$end_position)
-  names(semi_subread2) <- c("Gene_id","Symbol","SR_FPKM","SR_Cuartiles","SR_Counts","SR_locus")
+  names(semi_subread2) <- c("Gene_id","Symbol","SR_Counts","SR_FPKM","SR_Cuartiles","SR_locus")
   semi_subread2 <- semi_subread2[which(semi_subread2$SR_locus%in%grep("^C|^G",semi_subread2$SR_locus, value=TRUE, invert = TRUE)),]
 
   # merging
@@ -58,27 +54,23 @@ four_counter2summary <- function(semi_subread,semi_cuff,semi_quant,semi_htseq,
 
   mfinal <- m3_logico[m3_logico$Symbol%in%ngenes,]
 
-  head(mfinal)
-  mfinal2 <- mfinal[,c(1,2,15,6,11,19,5,9,18,3,7,12,16,4,8,13,14,17)]
-  head(mfinal2)
+  mfinal2 <- mfinal[,c("Symbol","Gene_id","Gene_id.x","SR_locus","ht_locus","cuff_locus","SR_Counts","Quan_Counts","ht_Counts",
+                       "SR_FPKM","Quan_FPKM","cuff_FPKM","ht_FPKM","SR_Cuartiles","Quan_Cuartiles","cuff_Cuartiles","ht_Cuartiles")]
 
   mfinal2$SR_Cuartiles <- as.numeric(as.character(mfinal2$SR_Cuartiles))
   mfinal2$Quan_Cuartiles <- as.numeric(as.character(mfinal2$Quan_Cuartiles))
   mfinal2$cuff_Cuartiles <- as.numeric(as.character(mfinal2$cuff_Cuartiles))
-  mfinal2$cuff2_Cuartiles <- as.numeric(as.character(mfinal2$cuff2_Cuartiles))
   mfinal2$ht_Cuartiles <- as.numeric(as.character(mfinal2$ht_Cuartiles))
 
-  mfinal2$dif_SR_Quan <- as.numeric(as.character(mfinal2$SR_Cuartiles)) - as.numeric(as.character(mfinal2$Quan_Cuartiles))
-  mfinal2$dif_SR_ht <- as.numeric(as.character(mfinal2$SR_Cuartiles)) - as.numeric(as.character(mfinal2$ht_Cuartiles))
-  mfinal2$dif_SR_cuff <- as.numeric(as.character(mfinal2$SR_Cuartiles)) - as.numeric(as.character(mfinal2$cuff_Cuartiles))
-  mfinal2$dif_SR_cuff2 <- as.numeric(as.character(mfinal2$SR_Cuartiles)) - as.numeric(as.character(mfinal2$cuff2_Cuartiles))
-
-  mfinal2$dif_Quan_ht <- as.numeric(as.character(mfinal2$Quan_Cuartiles)) - as.numeric(as.character(mfinal2$ht_Cuartiles))
-  mfinal2$dif_Quan_cuff <- as.numeric(as.character(mfinal2$Quan_Cuartiles)) - as.numeric(as.character(mfinal2$cuff_Cuartiles))
-  mfinal2$dif_Quan_cuff2 <- as.numeric(as.character(mfinal2$Quan_Cuartiles)) - as.numeric(as.character(mfinal2$cuff2_Cuartiles))
-  mfinal2$dif_ht_cuff <- as.numeric(as.character(mfinal2$ht_Cuartiles)) - as.numeric(as.character(mfinal2$cuff_Cuartiles))
-  mfinal2$dif_ht_cuff2 <- as.numeric(as.character(mfinal2$ht_Cuartiles)) - as.numeric(as.character(mfinal2$cuff2_Cuartiles))
-  mfinal2$dif_cuff_cuff2 <- as.numeric(as.character(mfinal2$cuff_Cuartiles)) - as.numeric(as.character(mfinal2$cuff2_Cuartiles))
+  if(dif_cuartiles)
+  {
+    mfinal2$dif_SR_Quan <- as.numeric(as.character(mfinal2$SR_Cuartiles)) - as.numeric(as.character(mfinal2$Quan_Cuartiles))
+    mfinal2$dif_SR_ht <- as.numeric(as.character(mfinal2$SR_Cuartiles)) - as.numeric(as.character(mfinal2$ht_Cuartiles))
+    mfinal2$dif_SR_cuff <- as.numeric(as.character(mfinal2$SR_Cuartiles)) - as.numeric(as.character(mfinal2$cuff_Cuartiles))
+    mfinal2$dif_Quan_ht <- as.numeric(as.character(mfinal2$Quan_Cuartiles)) - as.numeric(as.character(mfinal2$ht_Cuartiles))
+    mfinal2$dif_Quan_cuff <- as.numeric(as.character(mfinal2$Quan_Cuartiles)) - as.numeric(as.character(mfinal2$cuff_Cuartiles))
+    mfinal2$dif_ht_cuff <- as.numeric(as.character(mfinal2$ht_Cuartiles)) - as.numeric(as.character(mfinal2$cuff_Cuartiles))
+  }
 
   if(save_final)
   {
