8655e92d6878b2dc3a65ae9822f03298d5368040
itamuria <itamuria@gmail.com>
2022-07-04 16:53:57 +0200

    aldaketak

--- R/loop_for_merging.r
+++ R/loop_for_merging.r
@@ -1,17 +1,20 @@
 # Read all files and merge them
 
-directory <- "/home/itamayou/NASIR_RNA/ConteosTotales/uniendo/htseq_no"
-file_name <- "20210602_htseq_no_nasir.xlsx"
-remove_part_name <- "_htseq_counts_no_3"
+directory <- "/home/itamayou/SaraLabiano/005_Counts"
+file_name <- "20220704_sara_counts.xlsx"
+remove_part_name <- ".counts.txt"
 files_names <- dir(directory)
+setwd(directory)
 
-dat <- read.table(files_names[1])
+dat <- read.table(files_names[1], header = T)
+dat = dat[,c(1,7)]
 names(dat) <- c("ENSG",gsub(remove_part_name, "", files_names[1]))
 
 for(e in 2:length(files_names))
 {
   print(paste0(e, ": ", files_names[e]))
-  dat_temp <- read.table(files_names[e])
+  dat_temp <- read.table(files_names[e], header = T)
+  dat_temp = dat_temp[,c(1,7)]
   names(dat_temp) <- c("ENSG",gsub(remove_part_name, "", files_names[e]))
 
   dat <- merge(dat, dat_temp, by = "ENSG")
